34ba967c1ee05f139857164097c94ca3b1555929
brunodosax <brunodosax@hotmail.com>
2012-05-05 02:40:33 -0300

    Adicionado as funcionalidades de cadastro de tipo de sala e edição de tipo de sala http://localhost/rpv/gestaodisponibilidade/public/tipo-sala/index

--- gestaodisponibilidade/application/controllers/TipoSalaController.php
+++ gestaodisponibilidade/application/controllers/TipoSalaController.php
@@ -1,16 +1,53 @@
 <?php
 
-class TipoSalaController extends Zend_Controller_Action
-{
+class TipoSalaController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
-        // action body
+    public function indexAction() {
+        $modelTipoSala = new Application_Model_DbTable_TipoSala();
+        $listaTipoSala = $modelTipoSala->listaTipoSalaPor('descricao asc');
+        $this->view->listaTipoSala = $listaTipoSala;
+    }
+    
+    public function cadastrarTipoSalaAction() {
+        $form = new Application_Form_TipoSala();
+
+        if ($this->getRequest()->isPost()) {
+
+            if ($form->isValid($_POST)) {
+                $dados = $form->getValues();
+
+                $model = new Application_Model_DbTable_TipoSala();
+                $model->cadastraTipoSala($dados);
+
+                $this->_redirect('/tipo-sala/index');
+            }
+        }
+        $this->view->form = $form;
+    }
+
+    public function editarTipoSalaAction() {
+        $form = new Application_Form_TipoSala();
+
+        $numero = $this->getRequest()->getParam(Application_Model_DbTable_TipoSala::getPrimaryKeyName());
+
+        $tipoSalaModel = new Application_Model_DbTable_TipoSala();
+
+        $form->populate($tipoSalaModel->find($numero)->current()->toArray());
+
+        if ($this->getRequest()->isPost()) {
+            if ($form->isValid($_POST)) {
+                $dados = $form->getValues();
+                $tipoSalaModel->editarTipoSala($dados);
+                
+                $this->_redirect('/tipo-sala/index');
+            }
+        }
+
+        $this->view->form = $form;
     }
 
 
