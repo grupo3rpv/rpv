d70008decdef05a399e9a4e9455b1461bf610799
Marcelo Lopes <marcelomaialopes@yahoo.com.br>
2012-05-05 03:13:40 -0300

    manutenção edição de cadastro de sala

--- gestaodisponibilidade/application/models/DbTable/EquipamentoSala.php
+++ gestaodisponibilidade/application/models/DbTable/EquipamentoSala.php
@@ -33,11 +33,11 @@ class Application_Model_DbTable_EquipamentoSala extends Zend_Db_Table_Abstract {
     }
     
     public function getEquipamentosSala($numero_sala) {
-        $select = $this->select()->where('numero_sala = ? ', $numero_sala)
-                ->order('id_equipamento_sala asc');
-        var_dump($select);
-        die();
-        return $this->fetchRow($select);
+       
+        $select = $this->select()->where('numero_sala = ?', $numero_sala);
+       
+     
+        return $this->fetchAll($select);
     }
 
 }
