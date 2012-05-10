27155e618fcd878028f2299b6007d939469af718
Helison <Helison@10.2.9.238>
2012-05-10 12:15:01 -0300

    Crud curso pronto
    Sei que n precisava, mas soh sei fazer assim :P
    Professor n sei como faremos
    Probleminhas com exibir o curso
    Depois tem que ver uma forma de associar o curso ao professor ~~

---
+++ gestaodisponibilidade/application/controllers/CursoController.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CursoController
+ *
+ * @author Helison
+ */
+class CursoController extends Zend_Controller_Action {
+
+    public function init() {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction() {
+        $modelCurso = new Application_Model_DbTable_Curso();
+        $listaCurso = $modelCurso->listaCursoPor('nome asc');
+        $this->view->listarTodos = $listaCurso;
+    }
+
+
+    public function adicionarCursoAction() {
+        $form = new Application_Form_Curso();
+
+        if ($this->getRequest()->isPost()) {
+
+            if ($form->isValid($_POST)) {
+                $dados = $form->getValues();
+
+                $model = new Application_Model_DbTable_Curso();
+                $model->cadastraCurso($dados);
+
+                $this->_redirect('/curso/index');
+            }
+        }
+        $this->view->form = $form;
+    }
+
+    public function editarCursoAction() {
+        $form = new Application_Form_Curso();
+
+        $numero = $this->getRequest()->getParam(Application_Model_DbTable_Curso::getPrimaryKeyName());
+
+        $cursoModel = new Application_Model_DbTable_Curso();
+
+        $form->populate($cursoModel->find($numero)->current()->toArray());
+
+        if ($this->getRequest()->isPost()) {
+            if ($form->isValid($_POST)) {
+                $dados = $form->getValues();
+                $cursoModel->editarCurso($dados);
+
+                $this->_redirect('/curso/index');
+            }
+        }
+
+        $this->view->form = $form;
+    }
+
+    public function removerCursoAction() {
+        $id_curso = $this->_getParam('id_curso');
+        $cursoModel = new Application_Model_DbTable_Curso();
+        $cursoModel->removerCurso($id_curso);
+        $this->_redirect('/curso/index');
+    }
+
+}
+
