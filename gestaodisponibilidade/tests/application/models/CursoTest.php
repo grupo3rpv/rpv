115728a99824f6da632e6703b3837409e90c43be
Marcelo Lopes <marcelomaialopes@yahoo.com.br>
2012-05-10 17:12:30 -0300

    Trabalhando teste unitario do sprint

--- gestaodisponibilidade/tests/application/models/CursoTest.php
+++ gestaodisponibilidade/tests/application/models/CursoTest.php
@@ -21,6 +21,26 @@ class Application_Model_CursoTest extends PHPUnit_Framework_TestCase {
          $this->assertSame('23', $id);
          
      }
+     
+     public function testEditarCurso(){
+         $modelCurso = new Application_Model_DbTable_Curso();
+         $dados = array(
+             'id_curso'=>'23',
+             'nome'=>'Teste1 alterado',
+             'codigo'=>'12',
+         );
+         $id = $modelCurso->editarCurso($dados);
+         $cursoAlterado =  $modelCurso->find($id)->current();
+         $this->assertSame('Teste1 alterado', $cursoAlterado->getNome());
+     }
+     
+     public function testExcluirCurso(){
+         $modelCurso = new Application_Model_DbTable_Curso();
+         $numLinhas = $modelCurso->removerCurso('23');
+         $this->assertSame('1', $numLinhas);
+         
+     }
+     
     
 
 }
