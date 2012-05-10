dfaae492c289096cf72eba585dc8f1538fce3fb7
Marcelo Lopes <marcelomaialopes@yahoo.com.br>
2012-05-10 16:43:54 -0300

    Criando test unitario do model curso.

---
+++ gestaodisponibilidade/tests/application/models/CursoTest.php
@@ -0,0 +1,26 @@
+<?php
+
+class Application_Model_CursoTest extends PHPUnit_Framework_TestCase {
+
+    
+
+    public function setUp() {
+        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        parent::setUp();
+    }
+    
+    public function testCadastroCurso(){
+         $modelCurso = new Application_Model_DbTable_Curso();
+         $dados = array(
+             'id_curso'=>'23',
+             'nome'=>'Teste1',
+             'codigo'=>'12',
+         );
+         $id = $modelCurso->cadastraCurso($dados);
+     
+         $this->assertSame($id, 23);
+         
+     }
+    
+
+}
