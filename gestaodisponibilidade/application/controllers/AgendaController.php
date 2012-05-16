61f2f625ec328877e5d7e539433542c1603979b2
Thiago Cassio Krug <thiagockrug@gmail.com>
2012-05-16 16:20:22 -0300

    fullcalendar

---
+++ gestaodisponibilidade/application/controllers/AgendaController.php
@@ -0,0 +1,12 @@
+<?php
+
+class AgendaController extends Zend_Controller_Action {
+
+    public function init() {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction() {
+        
+    }
+}
