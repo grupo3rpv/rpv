df755f1411690f35dbd68bff747c2b81cdc1bea9
brunodosax <brunodosax@hotmail.com>
2012-08-28 21:52:42 -0300

    Criado a pagina de disponibilidade de horarios do professor funcionando a gravação no banco e ja ta puxando os selecionados

--- gestaodisponibilidade/application/models/DbTable/DisponibilidadeAula.php
+++ gestaodisponibilidade/application/models/DbTable/DisponibilidadeAula.php
@@ -10,29 +10,34 @@
  *
  * @author Bruno
  */
-class Application_Model_DbTable_DisponibilidadeAula extends Zend_Db_Table_Abstract{
+class Application_Model_DbTable_DisponibilidadeAula extends Zend_Db_Table_Abstract {
 
     protected $_rowClass = 'Application_Model_DisponibilidadeAula';
     protected $_name = 'disponibilidade_aula';
 
-    
-    public function gravarDados($id_usuario, $dia, $hora){
+    public function gravarDados($id_usuario, $dia, $hora) {
         $disponibilidade = $this->createRow();
-        /*@var $disponibilidade Application_Model_DisponibilidadeAula*/
+        /* @var $disponibilidade Application_Model_DisponibilidadeAula */
         $disponibilidade->setId_usuario($id_usuario);
         $disponibilidade->setDia($dia);
         $disponibilidade->setHora($hora);
-        
+
         $disponibilidade->save();
     }
+
+    public function removeDados($id_usuario, $dia, $hora) {
+        $this->delete('id_usuario = ' . $id_usuario . ' and dia = "' . $dia . '" and hora = "' . $hora . '"');
+    }
+
+    public function verificaCelulaSelecionada($id_usuario, $dia, $hora) {
+     $select = $this->select()->where('id_usuario = ' . $id_usuario . ' and dia = "' . $dia . '" and hora = "' . $hora . '"');
+        return $this->fetchAll($select)->count();
+        
+    }
     
-   public function removeDados($id_usuario, $dia, $hora){
-       $this->delete('id_usuario = '.$id_usuario.' and dia = '.$dia.' hora = '.$hora);
-   }
-    
-   public function verificaCelulaSelecionada($id_usuario, $dia, $hora){
-       $select = $this->select()->where('id_usuario = '.$id_usuario.' and dia = '.$dia.' hora = '.$hora);
-       $this->fetchAll($select)->count();
-   }
+    public function listaDisponibilidadesPorId($id_usuario){
+        $select = $this->select()->where('id_usuario = '.$id_usuario);
+       return $this->fetchAll($select);
+    }
 
 }
