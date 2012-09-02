2c3679e04c21f68fc70a7f1c93f5ae75973a03a1
ThiagoKrug <thiagockrug@gmail.com>
2012-09-02 19:10:43 -0300

    adição dos testes da ProfesorControllerTest

--- gestaodisponibilidade/tests/application/controllers/ProfessorControllerTest.php
+++ gestaodisponibilidade/tests/application/controllers/ProfessorControllerTest.php
@@ -11,11 +11,11 @@ class ProfessorControllerTest extends Zend_Test_PHPUnit_ControllerTestCase {
         $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
         parent::setUp();
     }
-    
+
     protected function tearDown() {
         parent::tearDown();
     }
-    
+
     public function testDisponibilidadeAulaAction() {
         $frontController = $this->getFrontController();
         $frontController->getRouter()->addDefaultRoutes();
@@ -26,33 +26,56 @@ class ProfessorControllerTest extends Zend_Test_PHPUnit_ControllerTestCase {
         $urlParams = $this->urlizeOptions($params);
         $url = $this->url($urlParams);
         $this->dispatch($url);
-        
+
         $this->assertModule($urlParams['module']);
         $this->assertController($urlParams['controller']);
         $this->assertAction($urlParams['action']);
     }
-    
-    public function testRecebeDisponibilidadeAulaAction(){
+
+    public function testRecebeDisponibilidadeAulaAction() {
         $frontController = $this->getFrontController();
         $frontController->getRouter()->addDefaultRoutes();
-        $params = array('action' => 'recebeDisponibilidadeAula',
+        $params = array('action' => 'recebe-disponibilidade-aula',
             'controller' => 'professor',
             'module' => 'default',
-            'id_usuario' => 5);
-        
+            'id_usuario' => 1,
+            'id' => '10:30:00-sexta');
+
         $this->request->setMethod('GET');
-        $this->request->setPost(array(
-            'id_usuario' => 5,
-            'id' => '10:30:00-segunda'
-        ));
-        
+
         $urlParams = $this->urlizeOptions($params);
         $url = $this->url($urlParams);
         $this->dispatch($url);
-        
+
         $this->assertModule($urlParams['module']);
         $this->assertController($urlParams['controller']);
         $this->assertAction($urlParams['action']);
+
+        $id_usuario = 1;
+        $dia = "sexta";
+        $dh = false;
+        $hora = "10:30:00";
+        $disponibilidadeAula = new Application_Model_DbTable_DisponibilidadeAula();
+        $disponibilidades = $disponibilidadeAula->listaDisponibilidadesPorId($id_usuario);
+        foreach ($disponibilidades as $disponibilidade) {
+            if ($disponibilidade->getHora() == $hora && $disponibilidade->getDia() == $dia) {
+                $dh = true;
+            }
+        }
+        $this->assertTrue($dh);
+        
+        $this->dispatch($url);
+        $this->assertModule($urlParams['module']);
+        $this->assertController($urlParams['controller']);
+        $this->assertAction($urlParams['action']);
+        
+        $disponibilidades = $disponibilidadeAula->listaDisponibilidadesPorId($id_usuario);
+        foreach ($disponibilidades as $disponibilidade) {
+            if ($disponibilidade->getHora() == $hora && $disponibilidade->getDia() == $dia) {
+                $dh = false;
+            }
+        }
+        $this->assertTrue($dh);
     }
 
 }
