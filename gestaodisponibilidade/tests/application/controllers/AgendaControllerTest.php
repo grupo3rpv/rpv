968b3c6c0a7546dcf9203f2639feec0e62170eec
Helisonrt <helisonreus@gmail.com>
2012-09-14 17:08:21 -0300

    esqueleto AgendaControllerTest
    inutil eu sei <o

---
+++ gestaodisponibilidade/tests/application/controllers/AgendaControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class AgendaControllerTest extends Zend_Test_PHPUnit_ControllerTestCase {
+
+    public function setUp() {
+        
+        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        parent::setUp();
+        
+    }
+    public function testAddEventoAction(){
+        
+    }
+    public function testCadastrarEventoAction(){
+        
+    }
+    public function testConfirmacaoEventoAction(){
+        
+    }
+    public function testRecusarEventoAction(){
+        
+    }
+    
+
+}
+
+?>
