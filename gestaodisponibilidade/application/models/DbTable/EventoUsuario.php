3b65fe6196a1e8c8c7880c3bcddf454b841081d5
Marcelo Lopes <marcelomaialopes@yahoo.com.br>
2012-09-14 17:35:22 -0300

    refatorando codigo

--- gestaodisponibilidade/application/models/DbTable/EventoUsuario.php
+++ gestaodisponibilidade/application/models/DbTable/EventoUsuario.php
@@ -34,57 +34,8 @@ class Application_Model_DbTable_EventoUsuario extends Zend_Db_Table_Abstract {
         
     }
     
-    public function eventoAceitoPorEmail($dados){
-        $eventoUsuario = $this->fetchRow('id_evento ="'.$dados['id_evento'].'" and id_professor = "'.$dados['id_professor'].'"');
-        
-        if(count($eventoUsuario)>0){
-             /*@var $eventoUsuario Application_Model_EventoUsuario*/
-             $eventoUsuario->setConvite('confirmado');
-             $eventoUsuario->save();
-             $eventoUsuarioProprietario = $this->fetchRow('id_evento ="'.$dados['id_evento'].'"  and convite="'.'proprietario'.'"');
-             $idProfProprietario = $eventoUsuarioProprietario->getId_professor();
-             $this->respostaEventoAceito($idProfProprietario, $dados['id_professor'], $dados['id_evento']);
-             
-         }
-        
-    }
     
-    public function eventoRecusadoPorEmail($dados){
-        $eventoUsuario = $this->fetchRow('id_evento ="'.$dados['id_evento'].'" and id_professor = "'.$dados['id_professor'].'"');
-        
-        if(count($eventoUsuario)>0){
-             /*@var $eventoUsuario Application_Model_EventoUsuario*/
-             $eventoUsuario->setConvite('recusado');
-             $eventoUsuario->save(); 
-         }
-        
-    }
     
-    public function respostaEventoAceito($idProfProprietario,$idProfConvidado,$idEvento){
-        $modelProfessor = new Application_Model_DbTable_Professor();
-        $professorProprietario = $modelProfessor->listaProfessorPorID($idProfProprietario);
-        $emailProprietario = $professorProprietario->getEmail();
-        $nomeProprietario = $professorProprietario->getNome();
-        
-        $professorConvidado = $modelProfessor->listaProfessorPorID($idProfConvidado);
-        $nomeProfessorConvidado = $professorConvidado->getNome();
-        
-        $modelEvento = new Application_Model_DbTable_Evento();
-        $evento = $modelEvento->find($idEvento)->current();
-                    $arrayEmail['convidado'] =$nomeProfessorConvidado;
-                    $arrayEmail['proprietarioEvento'] =$nomeProprietario;
-                    $arrayEmail['dia'] =$evento->getData_inicial();
-                    $arrayEmail['diaFinal'] =$evento->getData_final();
-                    $arrayEmail['horaInicial'] =$evento->getHora1();
-                    $arrayEmail['horaFinal'] =$evento->getHora2();
-        $view = new Zend_View();
-        $view->setScriptPath(APPLICATION_PATH . '/views/scripts');
-        $msg = $view->partial('templates/conviteeventoconfirmacao.phtml', $arrayEmail);
-        $mail = new Zend_Mail('utf-8');
-        $mail->addTo($emailProprietario)
-                    ->setBodyHtml($msg)
-                    ->setSubject('Resposta convite partipação de evento')
-                    ->send();
-       
-    }  
+   
+
 }
--- gestaodisponibilidade/application/models/EventoUsuario.php
+++ gestaodisponibilidade/application/models/EventoUsuario.php
@@ -32,6 +32,72 @@ class Application_Model_EventoUsuario extends Zend_Db_Table_Row_Abstract {
         $this->convite = $convite;
     }
 
+    public function eventoAceitoPorEmail($dados){
+        $modelEventoUsuario = new Application_Model_DbTable_EventoUsuario();
+        $eventoUsuario = $modelEventoUsuario->fetchRow('id_evento ="'.$dados['id_evento'].'" and id_professor = "'.$dados['id_professor'].'"');
+        
+        if(count($eventoUsuario)>0){
+             /*@var $eventoUsuario Application_Model_EventoUsuario*/
+             $eventoUsuario->setConvite('confirmado');
+             $eventoUsuario->save();
+             $eventoUsuarioProprietario = $modelEventoUsuario->fetchRow('id_evento ="'.$dados['id_evento'].'"  and convite="'.'proprietario'.'"');
+             $idProfProprietario = $eventoUsuarioProprietario->getId_professor();
+             $aceito = true;
+             $this->respostaEvento($idProfProprietario, $dados['id_professor'], $dados['id_evento'],$aceito);
+             
+         }
+        
+    }
+    
+    public function eventoRecusadoPorEmail($dados){
+        $modelEventoUsuario = new Application_Model_DbTable_EventoUsuario();
+        $eventoUsuario = $modelEventoUsuario->fetchRow('id_evento ="'.$dados['id_evento'].'" and id_professor = "'.$dados['id_professor'].'"');
+        
+        if(count($eventoUsuario)>0){
+             /*@var $eventoUsuario Application_Model_EventoUsuario*/
+             $eventoUsuario->setConvite('recusado');
+             $eventoUsuario->save();
+             $eventoUsuarioProprietario = $modelEventoUsuario->fetchRow('id_evento ="'.$dados['id_evento'].'"  and convite="'.'proprietario'.'"');
+             $idProfProprietario = $eventoUsuarioProprietario->getId_professor();
+             $aceito = false;
+             $this->respostaEvento($idProfProprietario, $dados['id_professor'], $dados['id_evento'],$aceito);
+             
+         }
+        
+    }
+    
+    public function respostaEvento($idProfProprietario,$idProfConvidado,$idEvento,$aceito){
+        $modelProfessor = new Application_Model_DbTable_Professor();
+        $professorProprietario = $modelProfessor->listaProfessorPorID($idProfProprietario);
+        $emailProprietario = $professorProprietario->getEmail();
+        $nomeProprietario = $professorProprietario->getNome();
+        
+        $professorConvidado = $modelProfessor->listaProfessorPorID($idProfConvidado);
+        $nomeProfessorConvidado = $professorConvidado->getNome();
+        
+        $modelEvento = new Application_Model_DbTable_Evento();
+        $evento = $modelEvento->find($idEvento)->current();
+                    $arrayEmail['convidado'] =$nomeProfessorConvidado;
+                    $arrayEmail['proprietarioEvento'] =$nomeProprietario;
+                    $arrayEmail['dia'] =$evento->getData_inicial();
+                    $arrayEmail['diaFinal'] =$evento->getData_final();
+                    $arrayEmail['horaInicial'] =$evento->getHora1();
+                    $arrayEmail['horaFinal'] =$evento->getHora2();
+        $view = new Zend_View();
+        $view->setScriptPath(APPLICATION_PATH . '/views/scripts');
+        if($aceito){
+        $msg = $view->partial('templates/conviteeventoconfirmacao.phtml', $arrayEmail);
+        }
+        else{
+        $msg = $view->partial('templates/conviteeventorecusado.phtml', $arrayEmail);    
+        }
+        $mail = new Zend_Mail('utf-8');
+        $mail->addTo($emailProprietario)
+                    ->setBodyHtml($msg)
+                    ->setSubject('Resposta convite partipação de evento')
+                    ->send();
+       
+    }
 
     
     
