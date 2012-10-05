568dea70e904247ece5410ffbd09a4159a36de60
ThiagoKrug <thiagockrug@gmail.com>
2012-10-05 17:59:48 -0300

    adição das permissões de usuario nos inits das controllers

--- gestaodisponibilidade/application/controllers/AreaProfessorController.php
+++ gestaodisponibilidade/application/controllers/AreaProfessorController.php
@@ -8,7 +8,17 @@
 class AreaProfessorController extends Zend_Controller_Action {
 
     public function init() {
-        /* Initialize action controller here */
+        if (Zend_Auth::getInstance()->hasIdentity()) {
+            $entity = Zend_Auth::getInstance()->getIdentity();
+            if ($entity->tipo_usuario == 'professor' ||
+                    $entity->tipo_usuario == 'coordenador' ||
+                    $entity->tipo_usuario == 'admin') {
+            } else {
+                $this->_redirect('/login/acesso-negado/');
+            }
+        } else {
+            $this->_redirect('/login/logar/');
+        }
     }
 
     public function indexAction() {
